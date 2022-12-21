1db3c9100d6fee6105cf1b874c7740c88af9f348
seo-K <dkasid@naver.com>
2022-12-21 20:16:34 +0900

    feat: mbti function

--- js/common.js
+++ js/common.js
@@ -9,11 +9,13 @@ let currentNumber = 0;
 let mbti = "";
 
 function renderQuestion() {
+  console.log(questions.length);
+  console.log(currentNumber);
+
   let value = (100 / questions.length) * (currentNumber + 1);
   const question = questions[currentNumber];
 
   progressValue.value = value;
-
   numberEl.innerHTML = question.number;
   questionEl.innerHTML = question.question;
   choice1El.innerHTML = question.choices[0].text;
@@ -21,20 +23,27 @@ function renderQuestion() {
 }
 
 function nextQuestions(choiceNumber) {
+  if (currentNumber === questions.length - 1) {
+    showResult();
+    return;
+  }
   const question = questions[currentNumber];
   mbti = mbti + question.choices[choiceNumber].value;
-  currentNumber = currentNumber + choiceNumber;
+  currentNumber = currentNumber + 1;
 
   renderQuestion();
 }
 
+function showResult() {
+  location.href = "/results.html?mbti=" + mbti; // 쿼리스트링 (기존 url에 값 전달)
+}
+
 choice1El.addEventListener("click", function () {
   nextQuestions(0);
-  console.log("눌림");
 });
 
 choice2El.addEventListener("click", function () {
   nextQuestions(1);
-  console.log("눌림");
 });
+
 renderQuestion();
